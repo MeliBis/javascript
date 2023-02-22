9d34836c35cf0d1cae769c86becb9601bfa80853
Melissa Barrios <melissa.blecona@gmail.com>
2023-02-22 12:36:51 -0600

    practica ecommerce

---
+++ dom/main.js
@@ -0,0 +1,7 @@
+let h1=document.querySelector('h1').innerHTML='soy un'
+let parrafito = document.querySelector('.parrafito').innerHTML='soy un'
+let señorParrafo = document.querySelector('#pid').innerHTML='hey'
+let input =document.querySelector('input')
+console.log({
+    h1,parrafito,señorParrafo,input
+});
